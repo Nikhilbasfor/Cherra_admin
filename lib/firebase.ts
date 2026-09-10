f24714e242f1943128dc524d5db9f714e2ed48a2
Theatre Mumbai <nikhilbasfor3@gmail.com>
2026-09-10 12:55:16 +0530

    feat: configure Firebase cherraproject SDK credentials in Admin

--- lib/firebase.ts
+++ lib/firebase.ts
@@ -13,17 +13,17 @@ import { Hotel, InquiryLead } from "./types";
 import { INITIAL_HOTELS, INITIAL_LEADS } from "./initialData";
 
 const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY || "",
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN || "",
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID || "",
-  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET || "",
-  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID || "",
-  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID || "",
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY || "AIzaSyCqJAgcMfdnyIzG-zFY3mdJDJxEO04tj-I",
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN || "cherraproject.firebaseapp.com",
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID || "cherraproject",
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET || "cherraproject.firebasestorage.app",
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID || "528185893048",
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID || "1:528185893048:web:6f359608b13a55a515d44d",
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID || "G-726185CDW3",
 };
 
 const isFirebaseConfigured = Boolean(
-  process.env.NEXT_PUBLIC_FIREBASE_API_KEY &&
-    process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID
+  firebaseConfig.apiKey && firebaseConfig.projectId
 );
 
 let app: ReturnType<typeof initializeApp> | null = null;
